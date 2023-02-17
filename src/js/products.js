32197d681082441e87658a73d444de09cc1f73b1
DINH THE CUONG <106668267+DinhTheCuong@users.noreply.github.com>
2023-02-18 02:07:55 +0700

    update4

--- src/js/products.js
+++ src/js/products.js
@@ -8,57 +8,57 @@ const renderProducts = {
             id: 1,
             name: 'đầm tiệc trễ vai',
             img: '../img/products/ao_1.jpg',
-            // path:
-            price: 50,
+            path: '../html/product.html',
+            price: '50.000',
         },
         {
             id: 2,
             name: 'áo vest dạ đỏ',
             img: '../img/products/ao_2.jpg',
             // path:
-            price: 80,
+            price: '80.000',
         },
         {
             id: 3,
             name: 'áo khoác kẻ nâu',
             img: '../img/products/ao_3.jpg',
             // path:
-            price: 100,
+            price: '100.000',
         },
         {
             id: 4,
             name: 'đầm liền thiết kế',
             img: '../img/products/ao_4.jpg',
             // path:
-            price: 120,
+            price: '120.000',
         },
         {
             id: 5,
             name: 'quần baggy hồng',
             img: '../img/products/ao_5.jpg',
             // path:
-            price: 50,
+            price: '50.000',
         },
         {
             id: 6,
             name: 'quần culottes đen',
             img: '../img/products/ao_6.jpg',
             // path:
-            price: 80,
+            price: '80.000',
         },
         {
             id: 7,
             name: 'đầm đỏ thiết kế',
             img: '../img/products/ao_7.jpg',
             // path:
-            price: 100,
+            price: '100.000',
         },
         {
             id: 8,
             name: 'đầm tiệc nhung',
             img: '../img/products/ao_8.jpg',
             // path:
-            price: 120,
+            price: '120.000',
         }
     ],
 
@@ -66,7 +66,7 @@ const renderProducts = {
         const htmls = this.products.map((product, index) => {
             return `
                 <div class="product-card">
-                    <a class="product-link" href="">
+                    <a class="product-link" href="${product.path}">
                         <img class="product-image" src="${product.img}" alt="">
                         
                         
